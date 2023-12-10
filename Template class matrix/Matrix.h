bdd8ffad610afd013d9f1e551eab1f54c12ff85a
Андрей <dusha22075@gmail.com>
2023-12-10 14:01:44 +0300

    Добавьте файлы проекта.

---
+++ Template class matrix/Matrix.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#ifndef MATRIX_H
+#define MATRIX_H
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+template <typename T>
+class Matrix {
+private:
+	int rows;
+	int cols;
+	T** data;
+
+public:
+	Matrix(int rows, int cols);
+	~Matrix()
+
+	void fillFromKeyboard();
+	void fillWithRandomValues();
+	void display();
+
+	Matrix<T> operator+(const Matrix<T>& other);
+	Matrix<T> operator-(const Matrix<T>& other);
+	Matrix<T> operator*(const Matrix<T>& other);
+	Matrix<T> operator/(const Matrix<T>& other);
+
+	T findMaxElement();
+};
+#include "Matrix.cpp" // Подключаем файл с реализацией методов шаблонного класса Matrix
+
+#endif // MATRIX_H
---
+++ Template class matrix/main.cpp
@@ -0,0 +1,68 @@
+﻿#include<iostream>
+#include<Windows.h>
+#include"Matrix.h"
+
+
+using namespace std;
+
+// Создайте шаблонный класс матрица.Необходимо реализовать 
+// динамическое выделение памяти, очистку памяти,заполнение
+// матрицы с клавиатуры, заполнение случайными значениями,
+// отображение матрицы, арифметические операции с помощью
+// перегруженных операторов (+, –, *, / ),
+// поиск максимального элемента.
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+    // Создаем матрицы с типом int
+    Matrix<int> matrix1(2, 2);
+    Matrix<int> matrix2(2, 2);
+
+    // Заполняем первую матрицу с клавиатуры
+    cout << "Fill in the first matrix:" << endl;
+    matrix1.();
+
+    // Заполняем вторую матрицу случайными значениями
+    cout << "Fill in the second matrix with random values:" <<endl;
+    matrix2.fillWithRandomValues();
+
+    // Выводим обе матрицы на экран
+    cout << "First matrix:" << endl;
+    matrix1.display();
+    cout << "Second matrix:" << endl;
+    matrix2.display();
+
+    // Выполняем сложение матриц
+    Matrix<int> sumMatrix = matrix1 + matrix2;
+    cout << "Sum of matrices:" << endl;
+    sumMatrix.display();
+
+    // Находим максимальный элемент в первой матрице
+    int maxElement = matrix1.findMaxElement();
+    cout << "Max element in the first matrix: " << maxElement << endl;
+
+
+
+
+
+   // try {
+   //     Matrix<double> matrix;
+   //     matrix.setMatrix();
+   //     matrix.getMatrix();
+   // }
+   // catch (const exception&) {
+
+   // }
+   //// _getch();
+
+
+	/*Matrix<double> matrix;
+	matrix.setMatrix();
+	matrix.getMatrix();*/
+
+
+
+	return 0;
+}
