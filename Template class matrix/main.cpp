50576f8bd3377498159a0e915ffc59fdaefac15f
Андрей <dusha22075@gmail.com>
2023-12-10 15:53:09 +0300

    16:00

--- Template class matrix/main.cpp
+++ Template class matrix/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include<iostream>
 #include<Windows.h>
-#include"Matrix.h"
-
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -12,57 +12,133 @@ using namespace std;
 // перегруженных операторов (+, –, *, / ),
 // поиск максимального элемента.
 
+template <typename T>
+class Matrix {
+    int rows;
+    int cols;
+    T** data;
+public:
+    Matrix(int rows, int cols) : rows(rows), cols(cols) {
+        data = new T * [rows];
+        for (int i = 0; i < rows; ++i) {
+            data[i] = new T[cols];
+        }
+    }
+
+    ~Matrix() {
+        for (int i = 0; i < rows; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+    }
+
+    void fillFromKeyboard() {
+        std::cout << "Введите элементы матрицы:" << std::endl;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                cin >> data[i][j];
+            }
+        }
+    }
+
+    void fillWithRandomValues() {
+        srand((unsigned)time(NULL));
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                data[i][j] = rand() % 99;
+            }
+        }
+    }
+
+    void display() {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                cout << data[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    Matrix<T> operator+(const Matrix<T>& other) {
+        Matrix<T> result(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                result.data[i][j] = data[i][j] + other.data[i][j];
+            }
+        }
+        return result;
+    }
+    
+    Matrix<T>operator-(const Matrix<T>& other)
+    {
+        Matrix<T> subtraction(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                subtraction.data[i][j] = data[i][j] - other.data[i][j];
+            }
+        }
+        return subtraction;
+    }
+
+    Matrix<T> operator*(const Matrix<T>& other)
+    {
+        Matrix<T> multiplication(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                multiplication.data[i][j] = data[i][j] * other.data[i][j];
+            }
+        }
+        return multiplication;
+    }
+
+    Matrix<T> operator/(const Matrix<T>& other)
+    {
+        Matrix<T> division(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                division.data[i][j] = data[i][j] / other.data[i][j];
+            }
+        }
+        return division;
+    }
+
+    T findMaxElement() {
+        T maxElement = data[0][0];
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (data[i][j] > maxElement) {
+                    maxElement = data[i][j];
+                }
+            }
+        }
+        return maxElement;
+    }
+};
+
 int main()
 {
 	SetConsoleOutputCP(1251);
 
-    // Создаем матрицы с типом int
     Matrix<int> matrix1(2, 2);
     Matrix<int> matrix2(2, 2);
 
-    // Заполняем первую матрицу с клавиатуры
-    cout << "Fill in the first matrix:" << endl;
-    matrix1.();
+    cout << "Заполняем первую матрицу:" << endl;
+    matrix1.fillFromKeyboard();
 
-    // Заполняем вторую матрицу случайными значениями
-    cout << "Fill in the second matrix with random values:" <<endl;
+    cout << "Заполняем вторую матрицу случайными значениями:" <<endl;
     matrix2.fillWithRandomValues();
 
-    // Выводим обе матрицы на экран
-    cout << "First matrix:" << endl;
+    cout << "Первая матрица:" << endl;
     matrix1.display();
-    cout << "Second matrix:" << endl;
+    cout << "Вторая матрица:" << endl;
     matrix2.display();
 
-    // Выполняем сложение матриц
     Matrix<int> sumMatrix = matrix1 + matrix2;
-    cout << "Sum of matrices:" << endl;
+    cout << "Сумма матриц:" << endl;
     sumMatrix.display();
 
-    // Находим максимальный элемент в первой матрице
     int maxElement = matrix1.findMaxElement();
-    cout << "Max element in the first matrix: " << maxElement << endl;
-
-
-
-
-
-   // try {
-   //     Matrix<double> matrix;
-   //     matrix.setMatrix();
-   //     matrix.getMatrix();
-   // }
-   // catch (const exception&) {
-
-   // }
-   //// _getch();
-
-
-	/*Matrix<double> matrix;
-	matrix.setMatrix();
-	matrix.getMatrix();*/
-
-
+    cout << "Максимальный элемент в первой матрице: " << maxElement << endl;
 
 	return 0;
 }
